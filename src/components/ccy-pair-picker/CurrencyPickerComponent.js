dc5e1e1dd4c72b5fbc624bac001cd3d0678eafb2
mdperini15 <perinimichaeld@yahoo.com>
2019-08-10 20:47:40 -0400

    Initial working ccy pair get request

--- src/components/ccy-pair-picker/CurrencyPickerComponent.js
+++ src/components/ccy-pair-picker/CurrencyPickerComponent.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 export default class CurrencyPickerComponent extends React.Component {
   symbol = this.props.symbol;
+
+  // getCCY() {
+  //   return new Promise((resolve) => {
+  //     if (!this.ccyPairs) {
+  //       this.http
+  //         .get(this.serverUrl)
+  //         .toPromise()
+  //         .then((result: any) => {
+  //           this.ccyPairs = result;
+  //           resolve(this.ccyPairs);
+  //         });
+  //     } else {
+  //       resolve(this.ccyPairs);
+  //     }
+  //   });
+componentDidMount() {
+    // axios.get('http://jsonplaceholder.typicode.com/posts')
+    //axios.get('http://localhost:3333')
+    //http://localhost:3333
+
+    const instance = axios.create({
+      baseURL: 'http://localhost:3333',
+      timeout: 1000,
+      headers: {'userid': 'michael'}
+    })
+
+    // setHeaders: {
+    //   userid: 'michael'
+    // }
+    
+    instance.get('http://localhost:3333/currencypairs')
+        .then(res => {
+            const faqs = res.data.slice(0,10);
+            console.log(`ccy pairs ${JSON.stringify(faqs)}`)
+            this.setState({ faqs });
+        })
+}
+
 
   render () {
     return (
