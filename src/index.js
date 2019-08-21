656718e54de0fb357604f23aead683c6b04f4385
Michael D. Perini <perinimichaeld@yahoo.com>
2019-08-21 01:13:21 -0400

    hooks examples

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import * as serviceWorker from './serviceWorker';
 
 // sandbox hooks
 // import StateHookExample from './sandbox/hooks/state.hook.example';
-import EffectHookExample from './sandbox/hooks/effect.hook.example';
+// import EffectHookExample from './sandbox/hooks/effect.hook.example';
+import HooksExamples from './sandbox/hooks/hooks.examples';
 
 // ReactDOM.render(<PriceTileComponent />, document.getElementById('root'));
-ReactDOM.render(<EffectHookExample />, document.getElementById('root'));
+ReactDOM.render(<HooksExamples />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
