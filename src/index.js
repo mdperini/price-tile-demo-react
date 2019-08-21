f1f8347e63bde24db0e64e8790ebbaa5b285f223
Michael D. Perini <perinimichaeld@yahoo.com>
2019-08-21 00:32:08 -0400

    hooks exampled, very compelling

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import PriceTileComponent from './components/price-tile/PriceTileComponent'
 
-import WorkspaceComponent from './components/workspace/WorkspaceComponent'
+// import WorkspaceComponent from './components/workspace/WorkspaceComponent'
 import * as serviceWorker from './serviceWorker';
 
+// sandbox hooks
+// import StateHookExample from './sandbox/hooks/state.hook.example';
+import EffectHookExample from './sandbox/hooks/effect.hook.example';
+
 // ReactDOM.render(<PriceTileComponent />, document.getElementById('root'));
-ReactDOM.render(<WorkspaceComponent />, document.getElementById('root'));
+ReactDOM.render(<EffectHookExample />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
