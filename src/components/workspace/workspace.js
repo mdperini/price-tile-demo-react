fb7dfa101e24c474b5a8df297203dce242cdc8dc
Michael D. Perini <perinimichaeld@yahoo.com>
2019-08-28 23:12:45 -0400

    code reiview and cleanupo, workspace behavior still needs work

--- src/components/workspace/workspace.js
+++ src/components/workspace/workspace.js
@@ -29,160 +29,70 @@ export const Workspace = () => {
     getUserPerferences();
   }, []);
 
- 
-
   if (loading === true) {
-
     return <Loading />;
+  }
+  
+  const onAdd  = event => {
+    const v1 = uuid.v1();
+      
+    layoutConfig.push(
+    {
+      key: v1,
+      id: v1,
+      symbol: 'EURUSD'
+    });
 
+      saveUserPreferences(layoutConfig);  
   }
 
- 
-
-    const onAdd  = event => {
-
-      const v1 = uuid.v1();
-
- 
-
-      layoutConfig.push(
-
-        {
-
-          key: v1,
-
-          id: v1,
-
-          symbol: 'EURUSD'
-
-        });
-
- 
-
-        saveUserPreferences(layoutConfig);
-
+  const onSave  = params => {
+    const matchingLayout = layoutConfig.filter((x) => x.key === params.id);    
+    if (!matchingLayout) {
+      return;
     }
 
- 
+    matchingLayout.symbol = params.symbol;
+    saveUserPreferences(layoutConfig);
+  }
 
-    const removeLayout = (layouts, id) => {
+  const onRemoved  = id => {
+    const tmp = layoutConfig.filter((x) => x.key !== id);
+    saveUserPreferences(tmp);     
+  }
 
-      for( var i = 0; i < layouts.length; i++){
-
-        if ( layouts[i].key === id) {
-
-          layouts.splice(i, 1);
-
-        }
-
+  const renderPriceTiles = layoutConfig => {
+      if (!layoutConfig) {
+          return;
       }
 
- 
+      const priceTiles = layoutConfig.map((priceTile) => {
+      return (
+          <PriceTile
+              key={priceTile.key}
+              id={priceTile.key}
+              symbol={priceTile.symbol}
+              onClick={onRemoved}
+              onChange={onSave}
+              notional={2500} />
+        );
+      });     
 
-      return layouts;
+      return priceTiles;
+  }
 
-    }
-
- 
-
-    const onSave  = params => {
-
-      const matchingLayout = layoutConfig.filter((x) => x.key === params.id);    
-
-      if (!matchingLayout) {
-
-        return;
-
-      }
-
- 
-
-      const layouts = removeLayout(layoutConfig, params.id );
-
- 
-
-      layouts.push({'symbol': params.symbol, 'key': params.id  });
-
-      saveUserPreferences(layouts);
-
-    }
-
- 
-
-    const onRemoved  = id => {
-
-      const tmp = layoutConfig.filter((x) => x.key !== id);
-
-      saveUserPreferences(tmp);     
-
-    }
-
- 
-
-    const renderPriceTiles = layoutConfig => {
-
-        if (!layoutConfig) {
-
-            return;
-
-        }
-
- 
-
-        const priceTiles = layoutConfig.map((priceTile) => {
-
-          return (
-
-              <PriceTile
-
-                  key={priceTile.key}
-
-                  id={priceTile.key}
-
-                  symbol={priceTile.symbol}
-
-                  onClick={onRemoved}
-
-                  onChange={onSave}
-
-                  notional={2500} />
-
-          );
-
-        });     
-
-  
-
-        return priceTiles;
-
-    }
-
- 
-
-    return (
-
-      <div>
-
-        <div className="price-tiles">
-
-          {renderPriceTiles(layoutConfig)}
-
-          <span className="add" onClick={onAdd}>
-
-                <i className="fa fa-plus-circle fa-5x add-inner"></i>
-
-          </span>
-
-        </div>
-
-        <hr></hr>
-
-        <TransactionGrid></TransactionGrid>
-
-      </div>                    
-
-    );
-
+  return (
+    <div>
+      <div className="price-tiles">
+        {renderPriceTiles(layoutConfig)}
+        <span className="add" onClick={onAdd}>
+              <i className="fa fa-plus-circle fa-5x add-inner"></i>
+        </span>
+      </div>
+      <hr></hr>
+      <TransactionGrid></TransactionGrid>
+    </div>                    
+  );
 }
 
  
